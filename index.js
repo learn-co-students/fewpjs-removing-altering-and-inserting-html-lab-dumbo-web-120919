722fd6c35a57e1fb25dd9d8fe3358ecde53e0cf0
Radouane Bahi <radbahi@gmail.com>
2020-01-28 13:37:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let mainNode = document.querySelector('#main');
+mainNode.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion" inside'
